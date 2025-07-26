848c4b5628f98c0cdaa3508cad7bd9768e6dae77
Pranay <pranayvjadhao197@gmail.com>
2025-07-26 22:17:45 +0530

    Initial commit

---
+++ Relational.java
@@ -0,0 +1,10 @@
+class Relational
+{
+	public static void main(String args[])
+	{
+		int a=10,b=5,c=15,d=10;
+		System.out.println("a>b :"+(a>b));
+		System.out.println("a>c :"+(a>c));
+		System.out.println("a>d :"+(a>d));
+	}
+}
