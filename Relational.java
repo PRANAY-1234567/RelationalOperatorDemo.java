8a72a16e9980abce9f91d1b187e8a1f89f7767be
Pranay Jadhao <pranayvjadhao197@gmail.com>
2026-04-10 22:42:57 +0530

    Update Relational.java

--- Relational.java
+++ Relational.java
@@ -2,7 +2,7 @@ class Relational
 {
 	public static void main(String args[])
 	{
-		int a=20,b=5,c=15,d=10;
+		int a=20,b=10,c=15,d=10;
 		System.out.println("a>b :"+(a>b));
 		System.out.println("a>c :"+(a>c));
 		System.out.println("a>d :"+(a>d));
